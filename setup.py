1e5563e847b3cfb949940974516ec0c569939de7
Sam <swheeler111@gmail.com>
2019-01-23 09:23:42 -0500

    more pip testing

--- setup.py
+++ setup.py
@@ -1,17 +1,5 @@
 #!/usr/bin/env python3
-import os, sys
-from setuptools import setup, find_packages
-
-# https://github.com/pypa/pip/issues/4187#issuecomment-452862842
-def read_requirements():
-    """Parse requirements from requirements.txt."""
-    reqs_path = os.path.join('.', 'requirements.txt')
-
-    with open(reqs_path, 'r') as f:
-        requirements = [line.rstrip() for line in f]
-
-    return requirements
-
+from setuptools import setup
 
 setup(
     name="tap-chatitive",
@@ -21,11 +9,15 @@ setup(
     url="http://simondata.com",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_chatitive"],
-    install_requires=read_requirements(),
+    install_requires=[
+        "singer-python==5.2.0",
+        'requests==2.18.4',
+        "pendulum==1.2.0",
+        "tap-kit @ git+https://github.com/Radico/tap-kit.git@master"
+    ],
     entry_points="""
     [console_scripts]
     tap-chatitive=tap_chatitive:main
     """,
     packages=["tap_chatitive"],
     include_package_data=True,
-)
